4b6a46621f608a3d0b8d59e449994010f378e17d
Fabian0597 <54745918+Fabian0597@users.noreply.github.com>
2022-07-01 14:58:20 +0200

    simple model start bash file

--- DOMAIN_ADAPTION/start_model_pool.sh
+++ DOMAIN_ADAPTION/start_model_pool.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 #for looping through different models with different configurations
-feature_of_interest="C:s_ist/X"
-num_epochs=10
-GAMMA=1.8
+feature_of_interest="D:P_mech./X"
+num_epochs=30
+GAMMAs=(0 0.1)
+MMD_layer_activation_flag=( True True True False False False )
 
 nums_pool=(0 1 2 3)
 for num_pool in ${nums_pool[@]}; do
-    python3 main.py $feature_of_interest $num_epochs $GAMMA $num_pool
+    for GAMMA in ${GAMMAs[@]}; do
+        python3 main.py $feature_of_interest $num_epochs $GAMMA $num_pool ${MMD_layer_activation_flag[@]} 
+    done
 done
